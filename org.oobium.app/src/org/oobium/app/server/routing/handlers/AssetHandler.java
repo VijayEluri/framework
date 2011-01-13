9d1948251b16a8846722cddda5937888e917d727
jeremyd <jeremyd@jeremyd-laptop>
2011-01-13 09:29:31 -0700

    fixed logic flip regarding head responses

--- org.oobium.app/src/org/oobium/app/server/routing/handlers/AssetHandler.java
+++ org.oobium.app/src/org/oobium/app/server/routing/handlers/AssetHandler.java
@@ -19,26 +19,66 @@ import java.util.Date;
 import org.oobium.app.server.response.AssetResponse;
 import org.oobium.app.server.response.Response;
 import org.oobium.app.server.routing.RouteHandler;
+import org.oobium.app.server.routing.Router;
 import org.oobium.http.HttpRequest;
-import org.oobium.http.HttpRequest.Type;
 import org.oobium.http.constants.ContentType;
 import org.oobium.http.constants.Header;
+import org.oobium.http.constants.RequestType;
 import org.oobium.utils.Config.Mode;
 
 public class AssetHandler extends RouteHandler {
 
-	private final ClassLoader loader;
+	private final String assetPath;
+	private final ContentType type;
 	private final String length;
 	private final String lastModified;
 	
-	public AssetHandler(ClassLoader loader, String length, String lastModified) {
-		this.loader = loader;
+	public AssetHandler(Router router, String assetPath, ContentType type, String length, String lastModified) {
+		super(router);
+		this.assetPath = assetPath;
+		this.type = type;
 		this.length = length;
 		this.lastModified = lastModified;
 	}
 
-	@Override
-	public Response routeRequest(HttpRequest request) throws Exception {
+//	private URL getUrl() {
+//		String asset = request.getPath();
+//		int ix = asset.lastIndexOf('.');
+//		if(ix != -1) {
+//			ContentType type = ContentType.getFromExtension(asset.substring(ix + 1), ContentType.HTML);
+//			URL url = null;
+//			if(request.getType() != RequestType.HEAD) {
+//				ClassLoader loader = router.getService().getClass().getClassLoader();
+//				url = loader.getResource(asset);
+//				if(url == null) {
+//					switch(type) {
+//					case CSS:
+//						asset = "/styles" + asset;
+//						break;
+//					case HTML:
+//						asset = "/html" + asset;
+//						break;
+//					case IMG:
+//					case IMG_GIF:
+//					case IMG_ICO:
+//					case IMG_JPEG:
+//					case IMG_JPG:
+//					case IMG_PNG:
+//						asset = "/images" + asset;
+//						break;
+//					case JS:
+//						asset = "/scripts" + asset;
+//						break;
+//					}
+//					url = loader.getResource(asset);
+//				}
+//			}
+//			return new AssetResponse(request.getType(), type, url, length, lastModified);
+//		}
+//		return null;
+//	}
+
+	private boolean isNotModified(HttpRequest request) {
 		if(Mode.isNotDEV() && lastModified != null && lastModified.length() > 0) {
 			String ifMod = request.getHeader(Header.IF_MODIFIED_SINCE);
 			if(ifMod != null && ifMod.length() > 0) {
@@ -46,7 +86,7 @@ public class AssetHandler extends RouteHandler {
 					Date since = httpDate(ifMod);
 					Date last = httpDate(lastModified);
 					if(!since.after(last)) {
-						return Response.notModified(request.getType());
+						return true;
 					}
 				} catch(ParseException e) {
 					if(logger.isLoggingTrace()) {
@@ -56,39 +96,32 @@ public class AssetHandler extends RouteHandler {
 				}
 			}
 		}
-		String asset = request.getPath();
-		int ix = asset.lastIndexOf('.');
-		if(ix != -1) {
-			ContentType type = ContentType.getFromExtension(asset.substring(ix + 1), ContentType.HTML);
-			URL url = null;
-			if(request.getType() != Type.HEAD) {
-				url = loader.getResource(asset);
-				if(url == null) {
-					switch(type) {
-					case CSS:
-						asset = "/styles" + asset;
-						break;
-					case HTML:
-						asset = "/html" + asset;
-						break;
-					case IMG:
-					case IMG_GIF:
-					case IMG_ICO:
-					case IMG_JPEG:
-					case IMG_JPG:
-					case IMG_PNG:
-						asset = "/images" + asset;
-						break;
-					case JS:
-						asset = "/scripts" + asset;
-						break;
-					}
-					url = loader.getResource(asset);
-				}
-			}
-			return new AssetResponse(request.getType(), type, url, length, lastModified);
+		return false;
+	}
+	
+	@Override
+	public Response routeRequest(HttpRequest request) throws Exception {
+		if(isNotModified(request)) {
+			return Response.notModified(request.getType());
+		}
+		
+		RequestType requestType = request.getType();
+
+		if(requestType == RequestType.HEAD) {
+			return new AssetResponse(requestType, type, null, length, lastModified);
+		}
+
+		ClassLoader loader = router.getService().getClass().getClassLoader();
+		URL url = loader.getResource(assetPath);
+		if(url != null) {
+			return new AssetResponse(requestType, type, url, length, lastModified);
 		}
 		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return assetPath;
 	}
 	
 }
