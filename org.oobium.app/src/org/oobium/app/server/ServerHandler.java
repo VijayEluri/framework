a7845ed9af681557b66c4fb407ecdfb38878618d
jeremyd <jeremy@oobium.com>
2011-05-28 21:02:47 -0600

    added support for the HTTP Request Header: RANGE

--- org.oobium.app/src/org/oobium/app/server/ServerHandler.java
+++ org.oobium.app/src/org/oobium/app/server/ServerHandler.java
@@ -1,17 +1,13 @@
 package org.oobium.app.server;
 
 import static org.jboss.netty.handler.codec.http.HttpHeaders.isKeepAlive;
-import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
-import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.TRANSFER_ENCODING;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.*;
 import static org.jboss.netty.handler.codec.http.HttpMethod.DELETE;
 import static org.jboss.netty.handler.codec.http.HttpMethod.GET;
 import static org.jboss.netty.handler.codec.http.HttpMethod.HEAD;
 import static org.jboss.netty.handler.codec.http.HttpMethod.POST;
 import static org.jboss.netty.handler.codec.http.HttpMethod.PUT;
-import static org.jboss.netty.handler.codec.http.HttpResponseStatus.INTERNAL_SERVER_ERROR;
-import static org.jboss.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
-import static org.jboss.netty.handler.codec.http.HttpResponseStatus.NOT_MODIFIED;
-import static org.jboss.netty.handler.codec.http.HttpResponseStatus.OK;
+import static org.jboss.netty.handler.codec.http.HttpResponseStatus.*;
 import static org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 import static org.oobium.utils.DateUtils.httpDate;
 import static org.oobium.utils.StringUtils.blank;
@@ -19,6 +15,7 @@ import static org.oobium.utils.StringUtils.blank;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
 import java.net.URL;
 import java.text.ParseException;
 import java.util.Date;
@@ -36,6 +33,7 @@ import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
 import org.jboss.netty.handler.codec.http.HttpHeaders;
+import org.jboss.netty.handler.codec.http.HttpHeaders.Names;
 import org.jboss.netty.handler.codec.http.HttpRequest;
 import org.jboss.netty.handler.codec.http.HttpResponse;
 import org.jboss.netty.handler.codec.http.websocket.WebSocketFrameDecoder;
@@ -64,7 +62,12 @@ public class ServerHandler extends SimpleChannelUpstreamHandler {
 
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
-		logger.warn(e.getCause().getLocalizedMessage());
+		if(logger.isLoggingTrace()) {
+			logger.warn(e.getCause());
+		} else {
+			Throwable t = e.getCause();
+			logger.warn(t.getClass() + ": " + t.getLocalizedMessage());
+		}
 		e.getChannel().close();
 	}
 
@@ -105,6 +108,10 @@ public class ServerHandler extends SimpleChannelUpstreamHandler {
 	}
 
 	private void handleHttpRequest(final ChannelHandlerContext ctx, final Request request) {
+		if(logger.isLoggingDebug()) {
+			logger.debug(request.toString());
+		}
+		
 		if(request.getMethod() == POST) {
 			Object attr = request.getParameters().get("_method");
 			if(attr instanceof Attribute) {
@@ -199,20 +206,36 @@ public class ServerHandler extends SimpleChannelUpstreamHandler {
 	}
 
 	private ChannelFuture writePayload(Channel channel, StaticResponse response) {
+		return writePayload(channel, response, null);
+	}
+	
+	private ChannelFuture writePayload(Channel channel, StaticResponse response, int[] range) {
 		Object payload = response.getPayload();
 
 		if(payload instanceof ChannelBuffer) {
+			if(range != null) {
+				((ChannelBuffer) payload).setIndex(range[0], range[1]);
+			}
 			return channel.write(payload);
 		}
 		
 		if(payload instanceof String) {
+			if(range != null) {
+				payload = ((String) payload).substring(range[0], range[1]);
+			}
 			return channel.write(ChannelBuffers.copiedBuffer((String) payload, CharsetUtil.UTF_8));
 		}
 
 		if(payload instanceof File) {
 			// TODO better file server: http://docs.jboss.org/netty/3.2/xref/org/jboss/netty/example/http/file/HttpStaticFileServerHandler.html
 			try {
-				return channel.write(new ChunkedFile((File) payload));
+				if(range == null) {
+					payload = new ChunkedFile((File) payload);
+				} else {
+					RandomAccessFile raf = new RandomAccessFile((File) payload, "r");
+					payload = new ChunkedFile(raf, range[0], range[1]-range[0], 8192);
+				}
+				return channel.write(payload);
 			} catch(Exception e) {
 				throw new RuntimeException(e);
 			}
@@ -227,7 +250,16 @@ public class ServerHandler extends SimpleChannelUpstreamHandler {
 		}
 		
 		if(payload instanceof InputStream) {
-			return channel.write(new ChunkedStream((InputStream) payload));
+			if(range == null) {
+				payload = new ChunkedStream((InputStream) payload);
+			} else {
+				try {
+					payload = new ChunkedStreamRange((InputStream) payload, range);
+				} catch(Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return channel.write(payload);
 		}
 		
 		throw new UnsupportedOperationException("unsupported payload type: " + payload);
@@ -244,18 +276,41 @@ public class ServerHandler extends SimpleChannelUpstreamHandler {
 			response.setContent(null);
 			response.setHeader(CONTENT_LENGTH, 0);
 			future = channel.write(response);
-		} else {
+		}
+		else {
 			if(request.getMethod() == HEAD) {
 				response.setContent(null);
 				response.setHeader(CONTENT_LENGTH, 0);
 				future = channel.write(response);
-			} else {
-				if(!response.containsHeader(CONTENT_LENGTH) && !response.containsHeader(TRANSFER_ENCODING)) {
-					response.setHeader(CONTENT_LENGTH, response.getContent().readableBytes());
-				}
-				future = channel.write(response);
-				if(response instanceof StaticResponse) {
-					future = writePayload(channel, (StaticResponse) response);
+			}
+			else {
+				int[] range = getRange(request, response);
+				if(range == null) {
+					if(!response.containsHeader(CONTENT_LENGTH) && !response.containsHeader(TRANSFER_ENCODING)) {
+						ChannelBuffer content = response.getContent();
+						response.setHeader(CONTENT_LENGTH, (content != null) ? content.readableBytes() : 0);
+					}
+					future = channel.write(response);
+					if(response instanceof StaticResponse) {
+						future = writePayload(channel, (StaticResponse) response);
+					}
+				} else {
+					if(range[0] == -1) { // invalid range
+						response = new DefaultHttpResponse(HTTP_1_1, REQUESTED_RANGE_NOT_SATISFIABLE);
+						response.setContent(ChannelBuffers.copiedBuffer(REQUESTED_RANGE_NOT_SATISFIABLE.getReasonPhrase(), CharsetUtil.UTF_8));
+						future = channel.write(response);
+					} else {
+						response.setStatus(PARTIAL_CONTENT);
+						response.setHeader(CONTENT_LENGTH, range[1]-range[0]);
+						ChannelBuffer content = response.getContent();
+						if(content != null && content.readableBytes() > 0) {
+							content.setIndex(range[0], range[1]);
+						}
+						future = channel.write(response);
+						if(response instanceof StaticResponse) {
+							future = writePayload(channel, (StaticResponse) response, range);
+						}
+					}
 				}
 			}
 		}
@@ -268,4 +323,54 @@ public class ServerHandler extends SimpleChannelUpstreamHandler {
 		request.dispose(); // release any temporary files in the parameters
 	}
 
+	private int[] getRange(Request request, HttpResponse response) {
+		String header = request.getHeader(Names.RANGE);
+		if(header != null) {
+			String[] sa = header.split("\\s*=\\s*", 2);
+			if(sa.length == 2) {
+				sa = sa[1].split("\\s*,\\s*");
+				if(sa.length == 1) {
+					if(sa[0].length() == 0) {
+						logger.warn("empty range");
+					} else {
+						int clen;
+						header = response.getHeader(CONTENT_LENGTH);
+						if(header == null) {
+							ChannelBuffer content = response.getContent();
+							clen = (content != null) ? content.readableBytes() : 0;
+						} else {
+							clen = Integer.parseInt(header);
+						}
+						
+						if(sa[0].charAt(0) == '-') {
+							// bytes=-500
+							int len = Integer.parseInt(sa[0].substring(1));
+							return new int[] { (len > clen) ? 0 : (len-clen), clen };
+						}
+						else if(sa[0].charAt(sa[0].length()-1) == '-') {
+							// bytes=500-
+							int start = Integer.parseInt(sa[0].substring(0, sa[0].length()-1));
+							return new int[] { (start > clen) ? -1 : start, clen };
+						}
+						else {
+							// bytes=500-1000
+							sa = sa[0].split("\\s*-\\s*", 2);
+							int start = Integer.parseInt(sa[0]);
+							if(start < clen) {
+								int end = Integer.parseInt(sa[1]);
+								if(start < end) {
+									return new int[] { start, end };
+								}
+							}
+							return new int[] { -1, -1 };
+						}
+					}
+				} else {
+					logger.warn("multiple ranges not yet supported");
+				}
+			}
+		}
+		return null;
+	}
+	
 }
