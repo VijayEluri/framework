23ae04fb0f9b6bbc1d9713f6908022104fd4a4e9
jeremyd <jeremy@oobium.com>
2011-06-25 15:10:01 -0600

    removed obsolete "regen" parts

--- org.oobium.build/src/org/oobium/build/gen/ControllerGenerator.java
+++ org.oobium.build/src/org/oobium/build/gen/ControllerGenerator.java
@@ -10,8 +10,6 @@
  ******************************************************************************/
 package org.oobium.build.gen;
 
-import static org.oobium.build.util.SourceFile.closer;
-import static org.oobium.build.util.SourceFile.find;
 import static org.oobium.utils.FileUtils.writeFile;
 import static org.oobium.utils.StringUtils.camelCase;
 import static org.oobium.utils.StringUtils.packageName;
@@ -23,8 +21,6 @@ import static org.oobium.utils.StringUtils.varName;
 
 import java.io.File;
 import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.TreeSet;
 import java.util.regex.Matcher;
@@ -38,11 +34,6 @@ import org.oobium.app.http.Action;
 import org.oobium.app.http.MimeType;
 
 public class ControllerGenerator {
-
-	private static final char[] regen = {'@','R','e','g','e','n'};
-	private static final char[] CLASS = {'c','l','a','s','s'};
-	private static final char[] IMPORT = {'i','m','p','o','r','t'};
-	private static final char[] PACKAGE = {'p','a','c','k','a','g','e'};
 
 	private static final Matcher[] sigs = new Matcher[7];
 	{
@@ -82,10 +73,10 @@ public class ControllerGenerator {
 		src.imports.add(SQLException.class.getCanonicalName());
 		
 		StringBuilder sb = new StringBuilder();
-		sb.append("\t@Override\n");
-		sb.append("\tpublic void handleRequest() throws SQLException {\n");
-		sb.append("\t\t// TODO handle the request\n");
-		sb.append("\t}");
+		sb.append("@Override\n");
+		sb.append("public void handleRequest() throws SQLException {\n");
+		sb.append("\t// TODO handle the request\n");
+		sb.append("}");
 		src.methods.put("show", sb.toString());
 		
 		return src.toSource();
@@ -204,46 +195,21 @@ public class ControllerGenerator {
 			addImports(i, src.imports);
 		}
 		
-		src.methods.put("create", genCreate(false));
-		src.methods.put("update", genUpdate(false));
-		src.methods.put("destroy", genDestroy(false));
-		src.methods.put("showAll", genShowAll(false));
+		src.methods.put("create", genCreate());
+		src.methods.put("update", genUpdate());
+		src.methods.put("destroy", genDestroy());
+		src.methods.put("showAll", genShowAll());
 		if(withViews) {
-			src.methods.put("showEdit", genShowEdit(false));
-			src.methods.put("showNew", genShowNew(false));
+			src.methods.put("showEdit", genShowEdit());
+			src.methods.put("showNew", genShowNew());
 		}
-		src.methods.put("show", genShow(false));
+		src.methods.put("show", genShow());
 		
 		return src.toSource();
 	}
 	
-	private String doGenerate(StringBuilder sb) {
-		int start = find(sb, 0, sb.length(), regen);
-		if(start != -1) {
-			TreeSet<String> imports = loadImports(sb);
-			while(start != -1) {
-				int end = find(sb, start+regen.length, sb.length(), '{');
-				for(int i = 0; i < sigs.length; i++) {
-					sigs[i].reset(sb.substring(start, end+1));
-					if(sigs[i].matches()) {
-						end = closer(sb, end);
-						if(end != -1) {
-							addImports(i, imports);
-							sb.replace(start, end+1, genMethod(i, true));
-						}
-						break;
-					}
-				}
-				start = find(sb, end+1, sb.length(), regen);
-			}
-			writeImports(sb, imports);
-		}
-		return sb.toString();
-	}
-	
-	private String genCreate(boolean regen) {
+	private String genCreate() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(regen ? "@Regen\n\t" : "\t");
 		sb.append("@Override // POST/URL/[models]\n");
 		sb.append("\tpublic void create() throws SQLException {\n");
 		sb.append("\t\t").append(mType).append(" ").append(varName).append(" = ").append("param(\"").append(varName).append("\", new ").append(mType).append("());\n");
@@ -272,9 +238,8 @@ public class ControllerGenerator {
 		return sb.toString();
 	}
 	
-	private String genDestroy(boolean regen) {
+	private String genDestroy() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(regen ? "@Regen\n\t" : "\t");
 		sb.append("@Override // DELETE/URL/[models]/id\n");
 		sb.append("\tpublic void destroy() throws SQLException {\n");
 		sb.append("\t\t").append(mType).append(" ").append(varName).append(" = new ").append(mType).append("().setId(getId());\n");
@@ -296,22 +261,8 @@ public class ControllerGenerator {
 		return sb.toString();
 	}
 	
-	private String genMethod(int i, boolean regen) {
-		switch(i) {
-		case 0: return genCreate(regen);
-		case 1: return genDestroy(regen);
-		case 2: return genUpdate(regen);
-		case 3: return genShowAll(regen);
-		case 4: return genShowEdit(regen);
-		case 5: return genShowNew(regen);
-		case 6: return genShow(regen);
-		}
-		return null;
-	}
-	
-	private String genShow(boolean regen) {
+	private String genShow() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(regen ? "@Regen\n\t" : "\t");
 		sb.append("@Override // GET/URL/[models]/id\n");
 		sb.append("\tpublic void show() throws SQLException {\n");
 		sb.append("\t\t").append(mType).append(" ").append(varName).append(" = ").append(mType).append(".find(getId());\n");
@@ -330,9 +281,8 @@ public class ControllerGenerator {
 		return sb.toString();
 	}
 
-	private String genShowAll(boolean regen) {
+	private String genShowAll() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(regen ? "@Regen\n\t" : "\t");
 		sb.append("@Override // GET/URL/[models]\n");
 		sb.append("\tpublic void showAll() throws SQLException {\n");
 		sb.append("\t\tList<").append(mType).append("> ").append(varNamePlural).append(" = ").append(mType).append(".findAll();\n");
@@ -350,9 +300,8 @@ public class ControllerGenerator {
 		return sb.toString();
 	}
 
-	private String genShowEdit(boolean regen) {
+	private String genShowEdit() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(regen ? "@Regen\n\t" : "\t");
 		sb.append("@Override // GET/URL/[models]/id/edit\n");
 		sb.append("\tpublic void showEdit() throws SQLException {\n");
 		sb.append("\t\t").append(mType).append(" ").append(varName).append(" = ").append(mType).append(".find(getId());\n");
@@ -363,9 +312,8 @@ public class ControllerGenerator {
 		return sb.toString();
 	}
 	
-	private String genShowNew(boolean regen) {
+	private String genShowNew() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(regen ? "@Regen\n\t" : "\t");
 		sb.append("@Override // GET/URL/[models]/new\n");
 		sb.append("\tpublic void showNew() throws SQLException {\n");
 		sb.append("\t\t").append(mType).append(" ").append(varName).append(" = new ").append(mType).append("();\n");
@@ -374,9 +322,8 @@ public class ControllerGenerator {
 		return sb.toString();
 	}
 	
-	private String genUpdate(boolean regen) {
+	private String genUpdate() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(regen ? "@Regen\n\t" : "\t");
 		sb.append("@Override // PUT/URL/[models]/id\n");
 		sb.append("\tpublic void update() throws SQLException {\n");
 		sb.append("\t\t").append(mType).append(" ").append(varName).append(" = ").append("param(\"").append(varName).append("\", new ").append(mType).append("()).setId(getId());\n");
@@ -405,92 +352,4 @@ public class ControllerGenerator {
 		return sb.toString();
 	}
 	
-	private TreeSet<String> loadImports(StringBuilder sb) {
-		TreeSet<String> imports = new TreeSet<String>();
-		int stop = find(sb, 0, sb.length(), CLASS);
-		int start = find(sb, 0, stop, IMPORT);
-		while(start != -1) {
-			start += IMPORT.length;
-			while(Character.isWhitespace(sb.charAt(start)) && start < stop) {
-				start++;
-			}
-			int end = find(sb, start, stop, ';');
-			imports.add(sb.substring(start, end));
-			start = find(sb, end, stop, IMPORT);
-		}
-		return imports;
-	}
-
-	private void writeImports(StringBuilder sb, TreeSet<String> imports) {
-		if(imports.isEmpty()) {
-			return;
-		}
-		
-		int stop = find(sb, 0, sb.length(), CLASS);
-		if(stop == -1) {
-			return;
-		}
-		
-		int start = find(sb, 0, stop, PACKAGE);
-		if(start == -1) {
-			start = 0;
-		} else {
-			start = find(sb, start+1, stop, '\n') + 1;
-		}
-
-		int end = find(sb, start + IMPORT.length, stop, IMPORT);
-		if(end != -1) {
-			while(true) {
-				int i = find(sb, end + IMPORT.length, stop, IMPORT);
-				if(i != -1) {
-					end = i;
-				} else {
-					end = find(sb, end, stop, '\n') + 1;
-					break;
-				}
-			}
-		}
-		
-		TreeSet<String> statics = new TreeSet<String>();
-		for(Iterator<String> iter = imports.iterator(); iter.hasNext(); ) {
-			String imp = iter.next();
-			if(imp.startsWith("static ")) {
-				statics.add(imp);
-				iter.remove();
-			}
-		}
-		for(String imp1 : new HashSet<String>(statics)) {
-			if(imp1.endsWith(".*")) {
-				String s1 = imp1.substring(0, imp1.lastIndexOf('.'));
-				for(Iterator<String> iter = statics.iterator(); iter.hasNext(); ) {
-					String imp2 = iter.next();
-					if(imp1 != imp2) {
-						String s2 = imp2.substring(0, imp2.lastIndexOf('.'));
-						if(s1.equals(s2) || (s1 + "Model").equals(s2)) {
-							iter.remove();
-						}
-					}
-				}
-			}
-		}
-		
-		StringBuilder sb2 = new StringBuilder();
-		sb2.append('\n');
-		for(String imp : statics) {
-			sb2.append("import ").append(imp).append(";\n");
-		}
-		if(!statics.isEmpty() && !imports.isEmpty()) {
-			sb2.append('\n');
-		}
-		for(String imp : imports) {
-			sb2.append("import ").append(imp).append(";\n");
-		}
-		
-		if(end == -1) {
-			sb.insert(start, sb2.toString());
-		} else {
-			sb.replace(start, end, sb2.toString());
-		}
-	}
-
 }
