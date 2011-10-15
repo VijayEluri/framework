c196b6db03aa97d3494b065208779860d6327007
Jeremy Dowdall <jeremy.dowdall@oobium.com>
2011-10-14 19:50:22 -0600

    added Model#load tests

--- org.oobium.framework.tests/src/org/oobium/persist/db/LoadTests.java
+++ org.oobium.framework.tests/src/org/oobium/persist/db/LoadTests.java
@@ -10,9 +10,7 @@
  ******************************************************************************/
 package org.oobium.persist.db;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.oobium.framework.tests.dyn.DynClasses;
@@ -21,6 +19,108 @@ import org.oobium.persist.Model;
 
 public class LoadTests extends BaseDbTestCase {
 
+	@Test
+	public void testLoad() throws Exception {
+		DynModel am = DynClasses.getModel("AModel").addAttr("name", "String.class").addHasOne("bModel", "BModel.class");
+		DynModel bm = DynClasses.getModel("BModel").addAttr("name", "String.class").addHasOne("cModel", "CModel.class");
+		DynModel cm = DynClasses.getModel("CModel").addAttr("name", "String.class");
+		
+		migrate(am, bm, cm);
+		
+		persistService.executeUpdate("INSERT INTO c_models(name) VALUES(?)", "nameC1");
+		persistService.executeUpdate("INSERT INTO b_models(name,c_model) VALUES(?,?)", "nameB1", 1);
+		persistService.executeUpdate("INSERT INTO a_models(name,b_model) VALUES(?,?)", "nameA1", 1);
+
+		Model a = am.newInstance();
+		a.setId(1);
+		
+		assertTrue(a.isEmpty());
+		
+		a.load();
+		
+		assertTrue(a.isSet("name"));
+		assertEquals("nameA1", a.peek("name"));
+		assertTrue(a.isSet("bModel"));
+		assertEquals(bm.getModelClass(), a.peek("bModel").getClass());
+		assertTrue(((Model) a.peek("bModel")).isEmpty());
+	}
+	
+	@Test
+	public void testLoadFields() throws Exception {
+		DynModel am = DynClasses.getModel("AModel").addAttr("name", "String.class").addHasOne("bModel", "BModel.class").addHasOne("cModel", "CModel.class");
+		DynModel bm = DynClasses.getModel("BModel").addAttr("name", "String.class").addHasOne("cModel", "CModel.class");
+		DynModel cm = DynClasses.getModel("CModel").addAttr("name", "String.class");
+		
+		migrate(am, bm, cm);
+		
+		persistService.executeUpdate("INSERT INTO c_models(name) VALUES(?)", "nameC1");
+		persistService.executeUpdate("INSERT INTO b_models(name,c_model) VALUES(?,?)", "nameB1", 1);
+		persistService.executeUpdate("INSERT INTO a_models(name,b_model) VALUES(?,?)", "nameA1", 1);
+
+		Model a = am.newInstance();
+		a.setId(1);
+		
+		assertTrue(a.isEmpty());
+		
+		a.load("bModel");
+		
+		assertFalse(a.isSet("name"));
+		assertTrue(a.isSet("bModel"));
+		assertEquals(bm.getModelClass(), a.peek("bModel").getClass());
+		assertTrue(((Model) a.peek("bModel")).isSet("name"));
+		assertTrue(((Model) a.peek("bModel")).isSet("cModel"));
+		assertFalse(a.isSet("cModel"));
+
+		// make sure that it does overwrite loaded fields, but not existing ones
+		Object b = a.get("bModel");
+		a.set("bTest", b);
+		
+		a.load("bModel, cModel");
+		
+		assertNotSame(b, a.peek("bModel"));
+		assertSame(b, a.peek("bTest"));
+		assertTrue(a.isSet("cModel"));
+	}
+	
+	@Test
+	public void testLoadFieldsWithInclude() throws Exception {
+		DynModel am = DynClasses.getModel("AModel").addAttr("name", "String.class").addHasOne("bModel", "BModel.class").addHasOne("cModel", "CModel.class");
+		DynModel bm = DynClasses.getModel("BModel").addAttr("name", "String.class").addHasOne("cModel", "CModel.class");
+		DynModel cm = DynClasses.getModel("CModel").addAttr("name", "String.class");
+		
+		migrate(am, bm, cm);
+		
+		persistService.executeUpdate("INSERT INTO c_models(name) VALUES(?)", "nameC1");
+		persistService.executeUpdate("INSERT INTO b_models(name,c_model) VALUES(?,?)", "nameB1", 1);
+		persistService.executeUpdate("INSERT INTO a_models(name,b_model) VALUES(?,?)", "nameA1", 1);
+
+		Model a = am.newInstance();
+		a.setId(1);
+		
+		assertTrue(a.isEmpty());
+		
+		a.load("bModel:cModel");
+		
+		assertFalse(a.isSet("name"));
+		assertTrue(a.isSet("bModel"));
+		assertEquals(bm.getModelClass(), a.peek("bModel").getClass());
+		assertTrue(((Model) a.peek("bModel")).isSet("cModel"));
+		assertFalse(a.isSet("cModel"));
+
+		a = am.newInstance();
+		a.setId(1);
+		
+		assertTrue(a.isEmpty());
+		
+		a.load("bModel:cModel,cModel");
+		
+		assertFalse(a.isSet("name"));
+		assertTrue(a.isSet("bModel"));
+		assertEquals(bm.getModelClass(), a.peek("bModel").getClass());
+		assertTrue(((Model) a.peek("bModel")).isSet("cModel"));
+		assertTrue(a.isSet("cModel"));
+	}
+	
 	@Test
 	public void testLoadWithInclude() throws Exception {
 		DynModel am = DynClasses.getModel("AModel").addAttr("name", "String.class").addHasOne("bModel", "BModel.class");
@@ -37,13 +137,13 @@ public class LoadTests extends BaseDbTestCase {
 		a.setId(1);
 		a.load("include : bModel");
 		
-		assertEquals("nameA1", a.get("name"));
-		assertNotNull(a.get("bModel"));
-		assertEquals("nameB1", ((Model) a.get("bModel")).get("name"));
+		assertEquals("nameA1", a.peek("name"));
+		assertNotNull(a.peek("bModel"));
+		assertEquals("nameB1", ((Model) a.peek("bModel")).peek("name"));
 		
 		// BModel's CModel should exist but not be resolved
-		assertNotNull(((Model) a.get("bModel")).get("cModel"));
-		assertFalse(((Model) ((Model) a.get("bModel")).get("cModel")).isSet("name"));
+		assertNotNull(((Model) a.peek("bModel")).peek("cModel"));
+		assertFalse(((Model) ((Model) a.peek("bModel")).peek("cModel")).isSet("name"));
 	}
 	
 }
