5266f92e7e487bbce3eed4a1054a07adfd48906b
Jeremy Dowdall <jeremy.dowdall@oobium.com>
2012-03-13 14:55:22 -0600

    cleaned up and sorted

--- org.oobium.persist/src/org/oobium/persist/ModelList.java
+++ org.oobium.persist/src/org/oobium/persist/ModelList.java
@@ -71,11 +71,27 @@ public class ModelList<E extends Model> implements List<E> {
 		return true;
 	}
 	
+	public void add(int index, E e) {
+		members.add(index, e);
+		setOpposite(e);
+	}
+	
 	@Override
 	public boolean addAll(Collection<? extends E> c) {
 		if(members.addAll(c)) {
 			for(E o : c) {
 				setOpposite(o);
+			}
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c) {
+		if(members.addAll(index, c)) {
+			for(E e : c) {
+				setOpposite(e);
 			}
 			return true;
 		}
@@ -96,7 +112,7 @@ public class ModelList<E extends Model> implements List<E> {
 			ca.clear();
 		}
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	private void clearOpposite(Model object) {
 		if(object == null) {
@@ -120,21 +136,10 @@ public class ModelList<E extends Model> implements List<E> {
 	public boolean contains(Object o) {
 		return members.contains(o);
 	}
-	
+
 	@Override
 	public boolean containsAll(Collection<?> c) {
 		return members.containsAll(c);
-	}
-	
-	@Override
-	public boolean isEmpty() {
-		return members.isEmpty();
-	}
-
-	@Override
-	public Iterator<E> iterator() {
-		// TODO create a custom iterator - this exposes the internal members collection
-		return members.iterator();
 	}
 
 	@SuppressWarnings("unchecked")
@@ -146,6 +151,59 @@ public class ModelList<E extends Model> implements List<E> {
 		return members.remove(model);
 	}
 	
+	public E first() {
+		return members.isEmpty() ? null : members.get(0);
+	}
+	
+	@Override
+	public E get(int index) {
+		return members.get(index);
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		return members.indexOf(o);
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		return members.isEmpty();
+	}
+	
+	@Override
+	public Iterator<E> iterator() {
+		// TODO create a custom iterator - this exposes the internal members collection
+		return members.iterator();
+	}
+
+	public E last() {
+		return members.isEmpty() ? null : members.get(members.size()-1);
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		return members.lastIndexOf(o);
+	}
+
+	@Override
+	public ListIterator<E> listIterator() {
+		// TODO create a custom iterator - this exposes the internal members collection
+		return members.listIterator();
+	}
+
+	@Override
+	public ListIterator<E> listIterator(int index) {
+		// TODO create a custom iterator - this exposes the internal members collection
+		return members.listIterator(index);
+	}
+	
+	@Override
+	public E remove(int index) {
+		E orig = members.remove(index);
+		clearOpposite(orig);
+		return orig;
+	}
+
 	@Override
 	@SuppressWarnings("unchecked")
 	public boolean remove(Object o) {
@@ -161,7 +219,7 @@ public class ModelList<E extends Model> implements List<E> {
 			return false;
 		}
 	}
-	
+
 	@Override
 	@SuppressWarnings("unchecked")
 	public boolean removeAll(Collection<?> c) {
@@ -178,8 +236,8 @@ public class ModelList<E extends Model> implements List<E> {
 			}
 			return false;
 		}
-	}
-
+	};
+	
 	/**
 	 * Unsupported method
 	 * @throws UnsupportedOperationException
@@ -188,7 +246,22 @@ public class ModelList<E extends Model> implements List<E> {
 	public boolean retainAll(Collection<?> c) {
 		throw new UnsupportedOperationException("retainAll is not currently supported");
 	}
-	
+
+	public ModelList<E> reverse() {
+		if(members != null) {
+			ArrayUtils.reverse(members);
+		}
+		return this;
+	}
+
+	@Override
+	public E set(int index, E element) {
+		E orig = members.set(index, element);
+		clearOpposite(orig);
+		setOpposite(element);
+		return orig;
+	}
+
 	@SuppressWarnings("unchecked")
 	private void setOpposite(Model object) {
 		if(object == null) {
@@ -214,15 +287,16 @@ public class ModelList<E extends Model> implements List<E> {
 		// default: nothing to do, just exit
 		}
 	}
-	
-	public boolean save() {
-		// TODO
-		return false;
-	}
-	
+
 	@Override
 	public int size() {
 		return members.size();
+	}
+
+	@Override
+	public List<E> subList(int fromIndex, int toIndex) {
+		// TODO create a new ModelList?
+		return members.subList(fromIndex, toIndex);
 	}
 
 	@Override
@@ -238,140 +312,6 @@ public class ModelList<E extends Model> implements List<E> {
 	@Override
 	public String toString() {
 		return String.valueOf(owner) + " <- " + String.valueOf(members);
-	}
-
-
-	public boolean add(E...model) {
-		// TODO
-		return false;
-	}
-	
-	public boolean remove(E...model) {
-		// TODO
-		return false;
-	}
-	
-	public boolean destroy(E model) {
-		// TODO
-		return false;
-	}
-	
-	public boolean hasAny(E model) {
-		// TODO
-		return false;
-	}
-	
-	public boolean isEmpty(E model) {
-		// TODO
-		return false;
-	}
-	
-	public int count(E model) {
-		// TODO
-		return 0;
-	}
-
-	public E find(String where, Object...values) {
-		return null;
-	}
-	
-//	public ActiveSet<E> findAll() {
-//		// TODO
-//		return null;
-//	}
-//	
-//	public ActiveSet<E> findAll(String where, Object...values) {
-//		// TODO
-//		return null;
-//	}
-
-	public E findFirst() {
-		// TODO
-		return null;
-	}
-	
-	public E findLast() {
-		// TODO
-		return null;
-	}
-	
-	public E first() {
-		return members.isEmpty() ? null : members.get(0);
-	}
-	
-	public E last() {
-		return members.isEmpty() ? null : members.get(members.size()-1);
-	}
-
-	public ModelList<E> reverse() {
-		if(members != null) {
-			ArrayUtils.reverse(members);
-		}
-		return this;
-	}
-
-	public void add(int index, E e) {
-		members.add(index, e);
-		setOpposite(e);
-	};
-	
-	@Override
-	public boolean addAll(int index, Collection<? extends E> c) {
-		if(members.addAll(index, c)) {
-			for(E e : c) {
-				setOpposite(e);
-			}
-			return true;
-		}
-		return false;
-	}
-
-	@Override
-	public E get(int index) {
-		return members.get(index);
-	}
-
-	@Override
-	public E set(int index, E element) {
-		E orig = members.set(index, element);
-		clearOpposite(orig);
-		setOpposite(element);
-		return orig;
-	}
-
-	@Override
-	public E remove(int index) {
-		E orig = members.remove(index);
-		clearOpposite(orig);
-		return orig;
-	}
-
-	@Override
-	public int indexOf(Object o) {
-		return members.indexOf(o);
-	}
-
-	@Override
-	public int lastIndexOf(Object o) {
-		return members.lastIndexOf(o);
-	}
-
-	@Override
-	public ListIterator<E> listIterator() {
-		// TODO create a custom iterator - this exposes the internal members collection
-		return members.listIterator();
-	}
-
-	@Override
-	public ListIterator<E> listIterator(int index) {
-		// TODO create a custom iterator - this exposes the internal members collection
-		return members.listIterator(index);
-	}
-
-	@Override
-	public List<E> subList(int fromIndex, int toIndex) {
-		// TODO create a new ModelList?
-		return members.subList(fromIndex, toIndex);
 	}
 	
 	
